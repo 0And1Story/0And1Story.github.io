3ab1ceafdad0b9192a56b752045e9d5061fe0037
0And1Story <37762083+0And1Story@users.noreply.github.com>
2019-03-15 23:06:15 +0800

    Add files via upload

---
+++ BlogPostAssets/js/sidebar.js
@@ -0,0 +1,15 @@
+document.write('\
+<section id="sidebar"> \
+  <!--************************************************************************\
+Sidebar starts here. It contains a searchbox, sample ad image and 6 links\
+****************************************************************************-->\
+  <input type="text" placeholder="搜索文章" name="as_q">\
+  <!-- <div id="adimage"><img src="BlogPostAssets/images/adimage.jpg" alt=""/></div> -->\
+  <div id="adimage"><img src="https://s2.ax1x.com/2019/03/09/ApuQds.jpg" alt=""/></div>\
+  <nav>\
+	<ul>\
+	  <li><a href="jiu-jiu-sheng-fa-biao.html" title="jiu-jiu-sheng-fa-biao">九九乘法表</a></li>\
+	  <li><a href="fast-io.html" title="fast-io">快读</a></li>\
+	</ul>\
+  </nav>\
+</section>');
