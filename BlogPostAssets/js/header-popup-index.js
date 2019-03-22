a12844504b14e739c73a8dbf48a55263c697695b
0And1Story <witherman233@gmail.com>
2019-03-22 21:49:32 +0800

    Preparations of V1.0 pre 6
    更新一些新内容

---
+++ BlogPostAssets/js/header-popup-index.js
@@ -0,0 +1,17 @@
+document.write('\
+    <nav id="popup-menu">\
+      <div class="popup-button">\
+        友链<br>\
+        <a href="https://dottlewither.github.io/" target="_blank"><div class="popup-item">Dottle</div></a>\
+      </div>\
+      <div class="popup-button">\
+        关于<br>\
+        <a href="https://www.luogu.org/blog/0AND1STORY" title="洛谷博客" target="_blank"><div class="popup-item">Luogu</div></a>\
+        <a href="https://github.com/0And1Story" title="GitHub" target="_blank"><div class="popup-item">GitHub</div></a>\
+      </div>\
+      <div class="popup-button">\
+        文章<br>\
+        <a href="./BlogPostAssets/articles/article-list.html" title="Article"><div class="popup-item">所有文章</div></a>\
+      </div>\
+    </nav>\
+');
