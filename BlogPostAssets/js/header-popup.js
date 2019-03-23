1b2ff058557d4b126cbe9243f431c111e334636e
0And1Story <witherman233@gmail.com>
2019-03-23 11:33:02 +0800

    加入部分图标

--- BlogPostAssets/js/header-popup.js
+++ BlogPostAssets/js/header-popup.js
@@ -1,21 +1,21 @@
 document.write('\
     <nav id="popup-menu">\
       <div class="popup-button">\
-        友链<br>\
+        <i class="fas fa-link"></i>&nbsp;友链<br><i class="fas fa-angle-down"></i>\
         <a href="https://dottlewither.github.io/" target="_blank"><div class="popup-item">Dottle</div></a>\
       </div>\
       <div class="popup-button">\
-        关于<br>\
+        <i class="fas fa-info-circle"></i>&nbsp;关于<br><i class="fas fa-angle-down"></i>\
         <a href="#" title="关于此博客"><div class="popup-item">在开始之前</div></a>\
-        <a href="#" title="感谢"><div class="popup-item">感谢</div></a>\
-        <a href="#" title="大佬"><div class="popup-item">大佬</div></a>\
+        <a href="#" title="感谢"><div class="popup-item">感谢&nbsp;<i class="fas fa-heart"></i></div></a>\
+        <a href="#" title="大佬"><div class="popup-item">大佬&nbsp;<i class="fas fa-user-friends"></i></div></a>\
         <a href="https://www.luogu.org/blog/0AND1STORY" title="洛谷博客" target="_blank"><div class="popup-item">Luogu</div></a>\
-        <a href="https://github.com/0And1Story" title="GitHub" target="_blank"><div class="popup-item">GitHub</div></a>\
+        <a href="https://github.com/0And1Story" title="GitHub" target="_blank"><div class="popup-item">GitHub&nbsp;<i class="fab fa-github"></i></div></a>\
       </div>\
       <div class="popup-button">\
-        文章<br>\
-        <a href="article-list.html" title="Article"><div class="popup-item">所有文章</div></a>\
-        <a href="#" title="信息学奥赛"><div class="popup-item">OI</div></a>\
+        <i class="fas fa-pen-square"></i>&nbsp;文章<br><i class="fas fa-angle-down"></i>\
+        <a href="article-list.html" title="Article"><div class="popup-item"><i class="fas fa-globe-asia"></i>&nbsp;所有文章</div></a>\
+        <a href="#" title="信息学奥赛"><div class="popup-item"><i class="fas fa-code"></i>&nbsp;OI</div></a>\
       </div>\
     </nav>\
 ');
