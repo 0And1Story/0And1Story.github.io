f56c692291163296cb7849f07b2ae7533902ec90
0And1Story <witherman233@gmail.com>
2019-03-30 17:41:26 +0800

    加入一些新功能
    按钮半动态加载
    复制代码样式更改

--- BlogPostAssets/js/copy-code.js
+++ BlogPostAssets/js/copy-code.js
@@ -1,8 +1,18 @@
-function copyCode(num) {
-	var text=document.getElementsByClassName('hljs')[num].innerText;
+function copyCode(self) {
+	var text=self.parentNode.getElementsByClassName('hljs')[0].innerText;
 	var input=document.getElementById('copy-buffer');
 	input.value=text;
 	input.select();
 	document.execCommand('copy');
-	alert('复制成功！');
+	self.innerHTML='<i class="far fa-copy"></i>&nbsp;复制成功';
+	self.setAttribute('style', 'background-color: rgba(146,146,146,1.00);transition:0.2s;');
+	setTimeout(function() {
+		self.innerHTML='<i class="far fa-copy"></i>&nbsp;复制代码';
+		self.removeAttribute('style');
+	}, 500);
+}
+function initCopyButton() {
+	var buttons=document.getElementsByClassName('copy-button');
+	for (var i = 0; i < buttons.length; i ++)
+		buttons[i].setAttribute('onClick', 'copyCode(this)');
 }
