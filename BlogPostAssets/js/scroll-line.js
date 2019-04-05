7dbc478213c596094573f440da8416257294636d
0And1Story <witherman233@gmail.com>
2019-04-05 16:11:45 +0800

    修复页面滚动进度条的一个BUG

--- BlogPostAssets/js/scroll-line.js
+++ BlogPostAssets/js/scroll-line.js
@@ -1,12 +1,15 @@
-window.onscroll = function() {
+function updateScrollLine() {
 	var winTop = document.documentElement.scrollTop || document.body.scrollTop;
 	var docHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
 	var winHeight = document.documentElement.clientHeight || document.body.clientHeight;
 	var scrollLen = winTop / (docHeight-winHeight) * 100;
 	var scrollLine = document.getElementsByClassName('scroll-line')[0];
 	scrollLine.setAttribute('style', 'width: ' + scrollLen + '%;');
-};
+}
 
 function initScrollLine() {
 	document.body.innerHTML = '<div class="scroll-line"></div>\n' + document.body.innerHTML;
+	updateScrollLine();
 }
+
+window.onscroll = updateScrollLine;
